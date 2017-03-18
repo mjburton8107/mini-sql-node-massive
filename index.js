816ad5465e853425d3b461ee11308c1c99e483a2
MikeB <burton.mj@gmail.com>
2017-03-18 11:41:30 -0600

    finished

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var connectionString = "postgres://MikeB@localhost/sandbox";
+var controller = require("./controller.js");
+
+var massiveInstance = massive.connectSync({connectionString : connectionString});
 
 var app = module.exports = express();
+
+app.set('db', massiveInstance);
+
+var db = app.get('db');
+
+controller.getPlanes(app);
+
+
+
+// db.new_plane(function(err, planes){
+//   console.log(err, "plane added")
+// })
+
+// db.get_planes(function(err, planes){
+//   console.log(err, planes)
+// })
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
+  console.log("Successfully listening on : 3000")
 })
-
